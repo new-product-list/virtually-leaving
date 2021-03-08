2bb61936c04d7c181505dd1a1ae626a3d7379bbd
Shaun-Farrell <shaunfarrell@outlook.com>
2021-03-08 13:17:40 +0000

    feat: basic web-app added (#6)

---
+++ packages/web-app/src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+// import { Button } from "design-system";
+import "./App.css";
+// import "design-system/dist/index.css";
+
+// useReducer etc etc
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App-header">
+        <p>Create a virtual leaving card board...</p>
+        {/* <Button size="large" label="click me" backgroundColor="white" /> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
